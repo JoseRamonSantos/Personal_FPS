cf241d9e2021b08be2ff86686dc0d72d451cd400
JoseRamonSantos <43370084+JoseRamonSantos@users.noreply.github.com>
2022-02-15 10:14:13 +0100

    Remove deprecated version

--- Prototipo_FPS_DEPRECATED/Assets/Scripts/Blackboard.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Blackboard
-{
-	public static GameMode m_gameMode = GameMode.ONE_VS_ONE;
-}
--- Prototipo_FPS_DEPRECATED/Assets/Scripts/Enumerations.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public enum  GameMode
-{
-    ONE_VS_ONE
-} 
